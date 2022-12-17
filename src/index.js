c8a66627e3a50070fd805527cee458ce165bc667
Aser <ajhubero16@gmail.com>
2022-12-17 20:19:51 +0800

    environemnt variables

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./static/css/global.css";
 import App from "./components/App";
 
 window.process = {};
-
+console.log(process.env);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
